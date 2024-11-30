e1ae03a02cae15b7785f0df8e03eae22d52861c3
Amirmohammad <amirmmdjf2006@gmail.com>
2024-11-30 12:42:00 +0330

    Update 10 Azar

---
+++ src/app/cart/loading.tsx
@@ -0,0 +1,10 @@
+const LoadingCart = ()=>{
+    
+    return(
+        <div className="w-full h-full bg-white absolute flex justify-center items-center">
+            <p>Loading...</p>
+        </div>
+    )
+}
+
+export default LoadingCart
