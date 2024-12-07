4ca31e8d7212ec6167f40abfb2226a2b3b59066b
Amirmohammad <amirmmdjf2006@gmail.com>
2024-12-07 10:52:12 +0330

    Update 17 Azar

---
+++ src/app/shop/error.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+const ShopErrorPage = () =>{
+
+    return(
+        <div className="h-screen flex justify-center items-center">
+            <h1 className="mb-10">Request fail please try again after some seconds</h1>
+        </div>
+    )
+
+}
+
+export default ShopErrorPage;
